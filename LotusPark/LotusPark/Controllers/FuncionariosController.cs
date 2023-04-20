f15ff4fb053201deab461a75d4cf11fb3581e5cf
GoncaloAlpalhao <aluno23048@ipt.pt>
2023-04-20 16:16:37 +0100

    Criação scaffold para funcionários e validações de atributos

---
+++ LotusPark/LotusPark/Controllers/FuncionariosController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LotusPark.Data;
+using LotusPark.Models;
+
+namespace LotusPark.Controllers
+{
+    public class FuncionariosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FuncionariosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Funcionarios
+        public async Task<IActionResult> Index()
+        {
+              return _context.Funcionarios != null ? 
+                          View(await _context.Funcionarios.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Funcionarios'  is null.");
+        }
+
+        // GET: Funcionarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Funcionarios == null)
+            {
+                return NotFound();
+            }
+
+            var funcionarios = await _context.Funcionarios
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (funcionarios == null)
+            {
+                return NotFound();
+            }
+
+            return View(funcionarios);
+        }
+
+        // GET: Funcionarios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Funcionarios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,NIF,Telefone,Email,CodPostal,Morada,Cargo,DataNascimento")] Funcionarios funcionarios)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(funcionarios);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(funcionarios);
+        }
+
+        // GET: Funcionarios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Funcionarios == null)
+            {
+                return NotFound();
+            }
+
+            var funcionarios = await _context.Funcionarios.FindAsync(id);
+            if (funcionarios == null)
+            {
+                return NotFound();
+            }
+            return View(funcionarios);
+        }
+
+        // POST: Funcionarios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,NIF,Telefone,Email,CodPostal,Morada,Cargo,DataNascimento")] Funcionarios funcionarios)
+        {
+            if (id != funcionarios.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(funcionarios);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FuncionariosExists(funcionarios.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(funcionarios);
+        }
+
+        // GET: Funcionarios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Funcionarios == null)
+            {
+                return NotFound();
+            }
+
+            var funcionarios = await _context.Funcionarios
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (funcionarios == null)
+            {
+                return NotFound();
+            }
+
+            return View(funcionarios);
+        }
+
+        // POST: Funcionarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Funcionarios == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Funcionarios'  is null.");
+            }
+            var funcionarios = await _context.Funcionarios.FindAsync(id);
+            if (funcionarios != null)
+            {
+                _context.Funcionarios.Remove(funcionarios);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FuncionariosExists(int id)
+        {
+          return (_context.Funcionarios?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- LotusPark/LotusPark/Models/Funcionarios.cs
+++ LotusPark/LotusPark/Models/Funcionarios.cs
@@ -1,4 +1,8 @@
-﻿namespace LotusPark.Models {
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace LotusPark.Models {
     public class Funcionarios {
 
         public Funcionarios() {
@@ -8,13 +12,29 @@
         }
 
         public int Id { get; set; }
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
+
+        [DisplayName("Nome Funcionário")]
         public string Nome { get; set; }
+
         public int NIF { get; set; }
+
+        [DisplayName("Telemóvel")]
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
         public string Telefone { get; set; }
+
+        [EmailAddress(ErrorMessage = "O {0} não está corretamente escrito")]
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
         public string Email { get; set; }
+
+        [DisplayName("Código Postal")]
         public string CodPostal { get; set; }
+
         public string Morada { get; set; }
+
         public string Cargo { get; set; }
+
+        [DisplayName("Data de Nascimento")]
         public DateTime DataNascimento { get; set; }
 
         //*********************************************
