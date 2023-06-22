ff3222b09a76f13d2112aa9d150a73b7a31c9019
MarceloM <aluno23028@ipt.pt>
2023-06-22 15:21:00 +0100

    Vista em PT e userRoles
    Alteração para português de Portugal das vistas e adição de limitações de acordo com as funções de cada utilizador.

---
+++ LotusPark/LotusPark/Data/Migrations/20230622115719_test.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LotusPark.Data.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a",
+                column: "ConcurrencyStamp",
+                value: "1056f2eb-314b-4d30-975b-f7e941751d94");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c",
+                column: "ConcurrencyStamp",
+                value: "cf1d4795-caa7-4284-86b3-f106daee43e7");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f",
+                column: "ConcurrencyStamp",
+                value: "5b6b6297-2cf1-4c29-82e0-d95a1700b672");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a",
+                column: "ConcurrencyStamp",
+                value: "1e9cef46-78dd-497b-a843-1661df296e81");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c",
+                column: "ConcurrencyStamp",
+                value: "a2d5f337-2f63-4058-8970-65ba8d868b3f");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f",
+                column: "ConcurrencyStamp",
+                value: "cb56140c-7dc2-4a83-b28c-19dda4039235");
+        }
+    }
+}
